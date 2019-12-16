323b341472709e876141214bd2f637e159ac2d5d
Yversio <50660146+Yversio@users.noreply.github.com>
2019-12-16 16:29:53 +0100

    Update product_showcase.php

--- Vuurwerk_winkel/Ricardo/src/script/product_showcase.php
+++ Vuurwerk_winkel/Ricardo/src/script/product_showcase.php
@@ -5,7 +5,7 @@ $gebruikersnaam = "root";
 $password = "";
 $dbnaam = "vuurwerk_webshop";
 
-function ConnectDatabase($servernaam, $gebruikersnaam, $password, $dbnaam){
+function CreateDatabase($servernaam, $gebruikersnaam, $password, $dbnaam){
   try {
   $conn = new PDO("mysql:host=$servernaam;dbname=$dbnaam", $gebruikersnaam, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -23,7 +23,7 @@ function ShowProductenKnal($sqlknal, $resultknal){
   while($row = $resultknal->fetch()){
     echo "<div class='vuurwerk_product Knal'>";
     echo "<img src=" .$row['pr_url_afbeelding'] ." alt='Knal Product' style='width:100%;'>";
-    echo "<div class='beschrijving'>";
+    echo "<div class='beschrijving'";
     echo "<h4><b>" .$row['pr_naam'] ."</b></h4>";
     echo "<p>Dit is een knal product</p>";
     echo "<button>Add to cart</button>";
@@ -39,16 +39,50 @@ function ShowProductenSier($sqlsier, $resultsier){
   while($row = $resultsier->fetch()){
     echo "<div class='vuurwerk_product Sier'>";
     echo "<img src=" .$row['pr_url_afbeelding'] ." alt='Sier Product' style='width:100%;'>";
-    echo "<div class='beschrijving'>";
+    echo "<div class='beschrijving'";
     echo "<h4><b>" .$row['pr_naam'] ."</b></h4>";
     echo "<p>Dit is een sier product</p>";
-    echo "<button class='addcart_btn'>Add to cart</button>";
+    echo "<button>Add to cart</button>";
     echo "</div>";
     echo "</div>";
   }
 }
 
-ConnectDatabase($servernaam, $gebruikersnaam, $password, $dbnaam);
+$sqlkinder = "SELECT * FROM `producten` WHERE pr_categorie = 'Kinder'";
+$resultkinder = $conn->query($sqlkinder);
+
+function ShowProductenKinder($sqlkinder, $resultkinder){
+  while($row = $resultkinder->fetch()){
+    echo "<div class='vuurwerk_product Kinder'>";
+    echo "<img src=" .$row['pr_url_afbeelding'] ." alt='Sier Product' style='width:100%;'>";
+    echo "<div class='beschrijving'";
+    echo "<h4><b>" .$row['pr_naam'] ."</b></h4>";
+    echo "<p>Dit is een Kinder product</p>";
+    echo "<button>Add to cart</button>";
+    echo "</div>";
+    echo "</div>";
+  }
+}
+
+$sqlkorting = "SELECT * FROM `producten` WHERE pr_categorie = 'Korting'";
+$resultkorting = $conn->query($sqlkorting);
+
+function ShowProductenKorting($sqlkorting, $resultkorting){
+  while($row = $resultkorting->fetch()){
+    echo "<div class='vuurwerk_product Korting'>";
+    echo "<img src=" .$row['pr_url_afbeelding'] ." alt='Sier Product' style='width:100%;'>";
+    echo "<div class='beschrijving'";
+    echo "<h4><b>" .$row['pr_naam'] ."</b></h4>";
+    echo "<p>Op dit product zit korting</p>";
+    echo "<button>Add to cart</button>";
+    echo "</div>";
+    echo "</div>";
+  }
+}
+
+CreateDatabase($servernaam, $gebruikersnaam, $password, $dbnaam);
 ShowProductenKnal($sqlknal, $resultknal);
 ShowProductenSier($sqlsier, $resultsier);
+ShowProductenKinder($sqlkinder, $resultkinder);
+ShowProductenKorting($sqlkorting, $resultkorting);
 ?>
