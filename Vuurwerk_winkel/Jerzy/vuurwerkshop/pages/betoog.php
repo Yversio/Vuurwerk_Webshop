54c895ea25d9da0a5fe9f3cc7b722b1556b7376b
jerzyvdbosch2001 <58209223+jerzyvdbosch2001@users.noreply.github.com>
2020-01-29 11:00:24 +0100

    Add files via upload

---
+++ Vuurwerk_winkel/Jerzy/vuurwerkshop/pages/betoog.php
@@ -0,0 +1,30 @@
+<body>
+<h1>Nederlands:</h1>
+<p><b>Wel / Niet vuurwerk betoog</b><br>
+    <b>Mijn mening:</b><br>
+    Persoonlijk vind ik vuurwerk niet meer bijzonder, en vind ik dat het verboden moet worden tegenwoordig wordt er alleen maar dingen gesloopt en gehandeld in illegaal vuurwerk dus dat probleem pak je ook aan als vuurwerk verboden wordt in heel Nederland
+<br><br>
+    <b>Waarom moet het verboden worden?</b><br>
+  Omdat er met vuurwerk  alleen maar dingen mee worden gesloopt zoals prullenbakken, bushokjes, deuren, auto’s dit heeft zeker te maken met het illegale vuurwerk uit België Duitsland en Polen. Ook worden er vaak dieren slachtoffer van vuurwerk omdat mensen vuurwerk naar de dieren gooien. Huisdieren zijn meestal ook bang voor vuurwerk.  En ieder jaar zijn er weer heel veel mensen slachtoffer van vuurwerk mensen die hun oog kwijt raken of hun hand door het vuurwerk.
+    <br><br>
+    <b>De oplossing</b><br>
+    In Nederland moet in elke stad een vuurwerk show worden gegeven door professionals wat door de gemeente word georganiseerd zodat alles veilig verloopt en er geen gewonden meer vallen op oudejaarsavond en iedereen gelukkig en gezond het nieuwejaar ik kan gaan.
+
+</p>
+<br><br>
+<h1>English:</h1>
+<p><b>Yes / No to fireworks</b><br>
+    <b>My opinion:</b><br>
+    Personally, I no longer find fireworks special. And I think it should be forbidden, only things will be demolished, and acted in illegal fireworks so you also tackle that problem if fireworks are banned throughout the Netherlands
+
+    <br><br>
+    <b>Why should it be banned?</b><br>
+    Because fireworks only demolish things like trash cans, bus shelters, doors, cars this certainly has to do with the illegal fireworks from Belgium, Germany and Poland. Also, animals often fall victim to fireworks because people throw fireworks at the animals. Pets are also usually afraid of fireworks. And every year a lot of people are victims of fireworks, people who lose their eyes because of the fireworks.
+
+
+    <br><br>
+    <b>The solution</b><br>
+    In the Netherlands, a fireworks show must be given by professionals in every city. what is organized by the municipality so that everything runs safely and no more injured on New Year's Eve and everyone happy and healthy the new year that I can go.
+
+</p>
+</body>
